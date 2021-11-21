19395f7fd507e227e1f094e8f0600099adb26c20
Ashish Mathai <ashishsaju1@hotmail.com>
2021-11-21 22:47:02 +1300

    Add project files.

---
+++ NameForm/Controllers/NameController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NameForm.Models;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text.Json;
+
+namespace NameForm.Controllers
+{
+    public class NameController : Controller
+    {
+
+        public NameController()
+        {
+        }
+
+        public IActionResult GenerateFile(NameViewModel nameObject)
+        {
+
+            if(nameObject.FirstName != null && nameObject.LastName != null)
+            {
+                string firstName = nameObject.FirstName;
+                string lastName = nameObject.LastName;
+            }
+
+            string fileName = "nameOutput.json";
+
+            string jsonString = JsonConvert.SerializeObject(nameObject, Formatting.Indented);
+
+            using (TextWriter writer = new StreamWriter(fileName))
+            {
+                writer.WriteLine(jsonString);
+                writer.Close();
+            }
+
+            return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
---
+++ NameForm/Models/NameViewModel.cs
@@ -0,0 +1,9 @@
+namespace NameForm.Models
+{
+    public class NameViewModel
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+    }
+}
